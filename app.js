51e1f677e15e6885f4846af58347dae9dbec37db
Tiffany Fann <tiffanyfann@ip77-149.vpnip.cc.ntu.edu.tw>
2020-08-14 12:50:30 +0800

    port config

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.get('*', (req, res) => {
 });
 //*:對所有路徑而言
 
-let port = process.env.port || 3000
+let port = process.env.PORT || 3000
 
 app.listen(port, () => {  
     console.log('Listening on port 3000');  
